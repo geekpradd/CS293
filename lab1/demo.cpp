bebce7ecc550d2266d1890a4a9b5cb7dabe77bf9
geekpradd <geekpradd@gmail.com>
2020-08-13 17:50:58 +0530

    organize

---
+++ lab1/demo.cpp
@@ -0,0 +1,37 @@
+#include <iostream> 
+#include <fstream>
+using namespace std; 
+
+int josephus(int n, int k) 
+{ 
+	if (n == 1) 
+		return 1; 
+	else
+		/* The position returned by josephus(n - 1, k) 
+		is adjusted because the recursive call 
+		josephus(n - 1, k) considers the 
+		original position k % n + 1 as position 1 */
+		return (josephus(n - 1, k) + k-1) % n + 1; 
+} 
+
+// Driver Program to test above function 
+int main() 
+{ 
+	ofstream fout;
+	fout.open("test");
+	ofstream ff;
+	ff.open("sol");
+	fout << (4000-1)*199 << endl;
+	for (int i=2; i<=4000; ++i){
+		for (int j=2; j<=200; ++j){
+			fout << i << " " << j << endl;
+			ff << josephus(i, j) << endl;
+		}
+	}
+	fout.close();
+	ff.close();
+
+	return 0; 
+} 
+
+// This code is contributed by shubhamsingh10 
---
+++ lab1/tester.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+long long J(long long n, long long k){
+	if (n == 1LL)
+		return 0LL;
+	if (n < k){
+		long long recur = J(n-1, k);
+		long long killed = (k-1)%n;
+		return (recur+killed+1)%n;
+	}
+	else {
+		long long small_n = n - n/k;
+		long long recur = J(small_n, k);
+		long long left = n - n%k;
+		long long zero = n - left;
+		long long rem_add = (small_n - zero);
+		long long transformed = (recur + rem_add) % small_n;
+		return (transformed + transformed/(k-1));
+	}
+	
+}
+
+int main(){
+	int t; cin >> t;
+	for (int i=1; i<=t; ++i){
+		long long n; long long k; 
+		cin >> n;
+		cin >> k;
+		long long ans;
+		if (k == 1){
+			ans = n;
+		}
+		else {
+			ans = J(n, k) + 1;
+		}
+		cout << ans << endl;
+		// cout << "Case " << i << ": " << ans << endl;
+	}
+	
+	return 0;
+}
